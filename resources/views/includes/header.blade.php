360add3395718d57bc7c34c710feb70f5ceb9473
Izabel Rosén <izabel.rosen@chasacademy.se>
2017-12-15 10:39:26 +0100

    Modified header burger menu with CSS and small changes in blade file

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,5 @@
 <!-- Navbar in header -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +9,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js"></script>
     <title>Document</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -15,9 +17,10 @@
 
 </head>
 <body>
+
 <nav class="navbar is-transparent">
   <div class="navbar-brand">
-  <div class="navbar-start">
+  <div class="navbar-end">
     <div class="navbar-burger burger" data-target="navMenu" onclick="toggleBurger()">
       <span></span>
       <span></span>
@@ -75,6 +78,7 @@
 </body>
 </html>
 
+
 <!-- If a dropdown is wanted from top 250 add this:
        <div class="navbar-item has-dropdown is-hoverable">
             <a class="navbar-link" href="#">
@@ -90,3 +94,127 @@
           </div>
         </div>  
 -->
+
+<!--
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Slideout Demo</title>
+    <meta http-equiv="cleartype" content="on">
+    <meta name="MobileOptimized" content="320">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <style>
+      body {
+        width: 100%;
+        height: 100%;
+      }
+
+      .slideout-menu {
+        position: fixed;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        z-index: 0;
+        width: 256px;
+        overflow-y: scroll;
+        -webkit-overflow-scrolling: touch;
+        display: none;
+      }
+
+      .slideout-panel {
+        position: relative;
+        z-index: 1;
+        will-change: transform;
+      }
+
+      .slideout-open,
+      .slideout-open body,
+      .slideout-open .slideout-panel {
+        overflow: hidden;
+      }
+
+      .slideout-open .slideout-menu {
+        display: block;
+      }
+    </style>
+  </head>
+  <body>
+
+    <nav id="menu">
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+  <figure class="image is-48x48">
+  <img src="https://bulma.io/images/placeholders/48x48.png">
+</figure>
+<p class="user-review">Izabel Rosén</p>
+<p class="user-review-written">0 reviews written</p>
+
+
+
+        <hr class="navbar-divider">
+
+        <a class="navbar-item" href="#">
+          <span class="icon">
+            <i class="fa fa-home"></i>
+          </span>
+            Home
+        </a>
+        <a class="navbar-item" href="#">
+        <span class="icon">
+          <i class="fa fa-star"></i>
+        </span>
+        Top 250
+        </a>
+        <a class="navbar-item" href="#">
+        <span class="icon">
+          <i class="fa fa-trophy"></i>
+        </span>
+          Categories
+        </a>
+        <a class="navbar-item" href="#">
+        <span class="icon">
+          <i class="fa fa-television"></i>
+        </span>
+          My watchlist
+        </a>
+        <a class="navbar-item" href="#">
+        <span class="icon">
+          <i class="fa fa-power-off"></i>
+        </span>
+        Log out
+        </a>
+
+    </div>
+    </nav>
+
+    <main id="panel">
+      <header>
+        <button class="toggle-button">☰</button>
+        
+      </header>
+    </main>
+
+    <script src="dist/slideout.min.js"></script>
+    <script>
+      var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+      });
+
+      // Toggle button
+      document.querySelector('.toggle-button').addEventListener('click', function() {
+        slideout.toggle();
+      });
+    </script>
+
+  </body>
+</html>
