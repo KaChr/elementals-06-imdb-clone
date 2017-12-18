8e933a56e7bbed52c9f6b88959c37159cc3c4a9c
Izabel Rosén <izabel.rosen@chasacademy.se>
2017-12-18 16:06:43 +0100

    Delete a stylingsheet that is not being used in header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,7 +9,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js"></script>
     <title>Document</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
