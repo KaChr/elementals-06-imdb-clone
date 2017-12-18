9dbf28fd6878a0a733d37ac4954e0bb1b8474e39
Izabel Rosén <izabel.rosen@chasacademy.se>
2017-12-18 16:02:02 +0100

    Install a JS package that is not being used atm

--- public/js/app.js
+++ public/js/app.js
@@ -753,7 +753,6 @@ module.exports = __webpack_require__(43);
 /* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -763,29 +762,6 @@ module.exports = __webpack_require__(43);
 __webpack_require__(9);
 
 __webpack_require__(34);
-document.addEventListener('DOMContentLoaded', function () {
-
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
-  }
-});
 
 /***/ }),
 /* 9 */
