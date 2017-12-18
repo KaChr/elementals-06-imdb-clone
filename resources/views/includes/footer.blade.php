1b413cc2a4f436f115c77d763f6bedfeb605feb7
joakimunge <ashba@unge.se>
2017-12-18 13:49:23 +0100

    Add logo to image folder

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <div class="container">
         <div class="content has-text-centered">
-            <p>&copy; elements 2017</p>
-            <p class="p__fig">MIT License<p>
+            <img class="footer__logo" src="{{ asset('images/emdb_logo.svg') }}" alt="" width="28px">
+            <p>&copy; elements 2017 <br /> MIT License</p>
             <hr>
             <h2><a href="https://github.com/chas-academy/elementals-06-imdb-clone"><i class="fa fa-github fa-3" aria-hidden="true"></i></a></h2>
         </div>
