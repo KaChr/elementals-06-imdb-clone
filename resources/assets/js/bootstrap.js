8b844a38b1acbf1c568acc8d2a3e4969abf50e7f
joakimunge <ashba@unge.se>
2017-12-18 21:20:50 +0100

    Require jQuery for slideout.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,10 +7,10 @@
  * code may be modified to fit the specific needs of your application.
  */
 
-// try {
-//     window.$ = window.jQuery = require('jquery');
+try {
+    window.$ = window.jQuery = require('jquery');
 
-// } catch (e) {}
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
