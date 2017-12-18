a27ef3cc349b68623623ff91f6071de0068fa0e3
joakimunge <ashba@unge.se>
2017-12-18 13:00:35 +0100

    Complete person block component

--- resources/views/includes/person.blade.php
+++ resources/views/includes/person.blade.php
@@ -37,3 +37,67 @@
         </div>
     </div>
 </section>
+<section class="columns">
+    <div class="column is-half-desktop">
+        <div class="columns is-mobile">
+            <div class="column is-narrow">
+                <div class="person__avatar">
+                    <img src="https://image.tmdb.org/t/p/w1280/bOlYWhVuOiU6azC4Bw6zlXZ5QTC.jpg" alt="avatar">
+                </div>
+            </div>
+            <div class="column">
+                <div>
+                    <h4 class="person__name">Keanu Reeves</h4>
+                    <p class="person__role">as John Wick</p>
+                </div>
+            </div>  
+        </div>
+    </div>
+    <div class="column is-half-desktop">
+        <div class="columns is-mobile">
+            <div class="column is-narrow">
+                <div class="person__avatar">
+                    <img src="https://image.tmdb.org/t/p/w1280/bOlYWhVuOiU6azC4Bw6zlXZ5QTC.jpg" alt="avatar">
+                </div>
+            </div>
+            <div class="column">
+                <div>
+                    <h4 class="person__name">Keanu Reeves</h4>
+                    <p class="person__role">as John Wick</p>
+                </div>
+            </div>  
+        </div>
+    </div>
+</section>
+<section class="columns">
+    <div class="column is-half-desktop">
+        <div class="columns is-mobile">
+            <div class="column is-narrow">
+                <div class="person__avatar">
+                    <img src="https://image.tmdb.org/t/p/w1280/bOlYWhVuOiU6azC4Bw6zlXZ5QTC.jpg" alt="avatar">
+                </div>
+            </div>
+            <div class="column">
+                <div>
+                    <h4 class="person__name">Keanu Reeves</h4>
+                    <p class="person__role">as John Wick</p>
+                </div>
+            </div>  
+        </div>
+    </div>
+    <div class="column is-half-desktop">
+        <div class="columns is-mobile">
+            <div class="column is-narrow">
+                <div class="person__avatar">
+                    <img src="https://image.tmdb.org/t/p/w1280/bOlYWhVuOiU6azC4Bw6zlXZ5QTC.jpg" alt="avatar">
+                </div>
+            </div>
+            <div class="column">
+                <div>
+                    <h4 class="person__name">Keanu Reeves</h4>
+                    <p class="person__role">as John Wick</p>
+                </div>
+            </div>  
+        </div>
+    </div>
+</section>
