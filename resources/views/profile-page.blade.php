f971d16be2d847ab5bc4200c2be6daef8baa6082
Izabel Rosén <izabel.rosen@chasacademy.se>
2017-12-19 16:57:37 +0100

    Create a profile page which includes head, header and footer

---
+++ resources/views/profile-page.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    @include('includes.head')
+    <body>
+        @include('includes.headertemp')
+
+
+<p> hej </p>
+
+
+
+@include('includes.footer')
+</body>
+</html>
