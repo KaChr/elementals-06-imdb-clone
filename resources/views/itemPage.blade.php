1e717f14ab5066da29dfcddad2c96eed7a86b540
Karin Christensen <karin.christensen@chasacademy.se>
2017-12-20 14:57:06 +0100

    Add reviews and head includes

--- resources/views/itemPage.blade.php
+++ resources/views/itemPage.blade.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="sv">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Bulma Link -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.css">
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/itemPage.css') }}">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
-    <title>Item page</title> 
+    @include('includes.head')
 </head>
 <body>
     <!-- <section class="item-info">
@@ -31,7 +20,7 @@
         <div class="container">
             <img class="poster-img" src="http://www.impawards.com/2014/posters/john_wick_xlg.jpg" alt="Movie poster">
         <article class="plot-text">
-            <h3>PLOT SUMMARY</h3>
+            <h4>PLOT SUMMARY</h4>
             <p>Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.</p>
             <!-- <a class="button is-outlined">READ MORE</a> -->
         </article>
@@ -39,12 +28,16 @@
     </section>
     <section class="cast-crew">
         <div class="container">
-      @include('includes.person')
+            @include('includes.person')
         </div>
     </section>
-
+    <section class="review">
+        <div class="container">
+            @include('includes.reviews')
+        </div>
+    </section>
     <footer>
-    
+        @include('includes.footer')
     </footer>
 </body>
 </html>
