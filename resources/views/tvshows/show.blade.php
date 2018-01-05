44136237b60c211f361a2069dd3b6e54d3e0d430
zavham <31238983+zavham@users.noreply.github.com>
2018-01-05 14:56:51 +0100

    some more code cleaned

--- resources/views/tvshows/show.blade.php
+++ resources/views/tvshows/show.blade.php
@@ -9,27 +9,6 @@
 <body>
 
     <h1>{{$tvshow->title}}</h1>
-    <!--<img src="{{$movie->poster}}">
-    <img src="http://image.tmdb.org/t/p/w1280{{$movie->movieBackdrop}}">;
-    <p>{{$movie->rating}}</p>
-    <p>{{$movie->summary}}</p>
-    <p>{{$movie->runtime}}</p>
-    <p>{{$movie->countries}}</p>
-    <h2>Genres</h2>
-    <p>
-    @foreach($item->genres as $genre)
-        {{ $loop->first ? '' : ', '}}
-        {{$genre->genre_title}}
-    @endforeach
-    </p>
-    <h2>Actors</h2>
-    @foreach($item->actors as $actor)
-    <p>{{$actor->name}}</p>
-    <img src="http://image.tmdb.org/t/p/w185{{$actor->profile_pic}}">;
-    @endforeach
-    <h2>Directors</h2>
-    @foreach($item->directors as $director)
-    <p>{{$director->name}}</p>
-    @endforeach -->
+    
 </body>
 </html>
