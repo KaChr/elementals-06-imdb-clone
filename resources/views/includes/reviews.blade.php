3bcaa6fa6bf08ffb361b1482e456dd7d46cd9fe4
Joakim Unge <ashba@unge.se>
2018-01-08 16:58:35 +0100

    Refactor reviews.blade.php

--- resources/views/includes/reviews.blade.php
+++ resources/views/includes/reviews.blade.php
@@ -6,13 +6,14 @@
     <span class="divider__line"></span>
 </div>
 <section class="columns">
+    @foreach($reviews as $review)
     <div class="column">
         <div class="columns is-mobile">
             <div class="column is-two-fifths-mobile">
                 <a href="">  
                     <div class="review__poster">
-                        <span class="review__rating">7.7</span>
-                        <img src="https://image.tmdb.org/t/p/original/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg" alt="">
+                        <span class="review__rating">{{ $review->review_rating }}</span>
+                        <img src="{{ $review->poster }}" alt="">
                     </div>
                 </a>
             </div>
@@ -22,42 +23,16 @@
                         <img src="https://avatars1.githubusercontent.com/u/24225542?s=460&v=4" alt="avatar">
                     </span>
                     <div>
-                        <h4 class="review__title">This is the title</h4>
-                        <p class="review__author">by Joakim Unge</p>
+                        <h4 class="review__title">{{ $review->title }}</h4>
+                        <p class="review__author">{{ $review->name }}</p>
                     </div>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Vestibulum in maximus turpis.
+                <p>
+                    {{ $review->body }}
                 </p>
                 <button class="button button--small button--solid-yellow">READ MORE</button>
             </div>  
         </div>
     </div>
-    <div class="column">
-        <div class="columns is-mobile">
-            <div class="column is-two-fifths-mobile">
-            <a href=""> 
-                <div class="review__poster">
-                    <span class="review__rating">8.2</span>
-                    <img src="https://image.tmdb.org/t/p/original/xGWVjewoXnJhvxKW619cMzppJDQ.jpg" alt="">
-                </div>
-            </a>
-            </div>
-            <div class="column">
-                <div class="review__info is-flex">
-                    <span class="review__avatar">
-                        <img src="https://avatars1.githubusercontent.com/u/24225542?s=460&v=4" alt="avatar">
-                    </span>
-                    <div>
-                        <h4 class="review__title">This is the title</h4>
-                        <p class="review__author">by Joakim Unge</p>
-                    </div>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Vestibulum in maximus turpis.
-                </p>
-                <button class="button button--small button--solid-yellow">READ MORE</button>
-            </div>  
-        </div>
-    </div>
+    @endforeach
 </section>
