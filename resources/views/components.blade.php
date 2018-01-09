9721d2af40a381f4d512fb1c30899d414ab140ce
Joakim Unge <ashba@unge.se>
2018-01-09 12:43:20 +0100

    Change includename

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
     @include('includes.head')
     <body>
-        @include('includes.headertemp')
+        @include('includes.navigation')
         <div id="app">
                 <section class="hero is-dark">
                     <div class="hero-body">
