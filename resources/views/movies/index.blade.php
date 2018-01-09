03336134d606418ca58ffe6ca6d6ebaf05ce2c39
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-01-09 12:54:50 +0100

    Continue with charts

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,34 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-    body{
-        column-count: 5;
-    }
-    div{
-        width: 300px;
-        margin: 0px;
-    }
-    a{
-        font-size: 26px;
-        display: inline-block;
-        width: 300px;
-    }
-    img{
-        width: 180px
-    }
-    </style>
-</head>
-<body>
-    @foreach($movies as $movie)
-    <div>
-    <a href="/movies/{{$movie->item_id}}">{{$movie->title}}</a>
-    <img src='{{$movie->poster}}'><br>
-    </div>
-    @endforeach
-</body>
+<html lang="{{ app()->getLocale() }}">
+    @include('includes.head')
+    <body>
+        @include('includes/navigation')
+        <h3 id="top-250">Top 250 Movies Right Now</h3>
+        <div class="grid-chart">
+            @foreach($movies->slice(0, 20) as $movie) 
+                <div> 
+                    <img src='https://www.mehlizmovies.is/movies/olafs-frozen-adventure{{$movie->poster}}'><br> 
+                    <a href="/movies/{{$movie->item_id}}">{{$movie->title}}</a> 
+                    <p>Rating: {{$movie->rating}}</p> 
+                </div> 
+            @endforeach 
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            <div>
+                <img src="http://p-hold.com/200/300">
+                <h4>Wide tile</h4>
+            </div>
+            
+
+        </div>
+        @include('includes/footer')
+    </body>
 </html>
