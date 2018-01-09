f5186eae622c0759579996a83f97defb098d79a8
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-01-09 15:14:26 +0100

    Small charts fix

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,79 +3,14 @@
     <body>
         @include('includes/navigation')
         <h3 id="top-250">Top 250 Movies Right Now</h3>
-        <div class="grid-chart">
+        <div class="grid--chart">
             @foreach($movies->slice(0, 20) as $movie) 
                 <div> 
-                    <img src='https://www.mehlizmovies.is/movies/olafs-frozen-adventure{{$movie->poster}}'><br> 
+                    <img src="{{$movie->poster}}"><br> 
                     <a href="/movies/{{$movie->item_id}}">{{$movie->title}}</a> 
                     <p>Rating: {{$movie->rating}}</p> 
                 </div> 
             @endforeach 
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            <div>
-                <img src="http://p-hold.com/200/300">
-                <h4>Wide tile</h4>
-            </div>
-            
 
         </div>
         @include('includes/footer')
