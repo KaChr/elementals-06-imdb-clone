4453d5be6523c5d15b304e9b308342d66f99645e
Joakim Unge <ashba@unge.se>
2018-01-09 12:44:28 +0100

    Refactor data in hero

--- resources/views/includes/hero.blade.php
+++ resources/views/includes/hero.blade.php
@@ -1,18 +1,18 @@
-<a href="/movies/{{ $featured->item_id }}">
-  <section class="hero is-light featured" style="background-image: url(http://image.tmdb.org/t/p/w1920{{$featured->movieBackdrop}})">
+<a href="/movies/{{ $featured[0]->item_id }}">
+  <section class="hero is-light featured" style="background-image: url(http://image.tmdb.org/t/p/w1920{{$featured[0]->movieBackdrop}})">
     <div class="hero-body is-flex">
         <div class="featured__content--bottom is-flex">
           <div class="featured__info">
             <span class="featured__info-genre">
-            @foreach($item->genres as $genre)
+            @foreach($item[0]->genres as $genre)
               {{ $loop->first ? '' : ', '}}
               {{$genre->genre_title}}
             @endforeach
             </span>
-            <h1 class="featured__info-title">{{ $featured->title }}</h1>
+            <h1 class="featured__info-title">{{ $featured[0]->title }}</h1>
           </div>
           <div class="featured__rating is-flex">
-            <span>{{ $featured->rating }}</span>
+            <span>{{ $featured[0]->rating }}</span>
           </div>
         </div>
     </div>
