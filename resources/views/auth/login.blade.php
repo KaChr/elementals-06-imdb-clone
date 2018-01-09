7d482fb4bb4f4ceab17e42da94939e0cc9852387
Keejv <kaveh.haddad@chasacademy.se>
2018-01-09 15:08:29 +0100

    Add new style to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,22 +2,12 @@
 
 @section('content')
 
-    <section class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    Login
-                </h1>
-            </div>
-        </div>
-    </section>
-
     <div class="columns is-marginless is-centered">
         <div class="column is-5">
             <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">Login</p>
-                </header>
+                
+                    <p>Login</p>
+                
 
                 <div class="card-content">
                     <form class="login-form" method="POST" action="{{ route('login') }}">
@@ -25,14 +15,19 @@
 
                         <div class="field is-horizontal">
                             <div class="field-label">
-                                <label class="label">E-Mail Address</label>
                             </div>
 
                             <div class="field-body">
                                 <div class="field">
-                                    <p class="control">
-                                        <input class="input" id="email" type="email" name="email"
+                                    <p class="control has-icons-left has-icons-right">
+                                        <input class="input" id="email" type="email" name="email" placeholder="Email"
                                                value="{{ old('email') }}" required autofocus>
+                                               <span class="icon is-small is-left">
+                                                 <i class="fa fa-envelope"></i>
+                                                </span>
+                                                <span class="icon is-small is-right">
+                                                 <i class="fa fa-check"></i>
+                                                 </span>
                                     </p>
 
                                     @if ($errors->has('email'))
@@ -46,13 +41,15 @@
 
                         <div class="field is-horizontal">
                             <div class="field-label">
-                                <label class="label">Password</label>
                             </div>
 
                             <div class="field-body">
                                 <div class="field">
-                                    <p class="control">
-                                        <input class="input" id="password" type="password" name="password" required>
+                                    <p class="control has-icons-right">
+                                        <input class="input" id="password" type="password" name="password" placeholder="Password" required>
+                                        <span class="icon is-small is-left">
+                                         <i class="fa fa-lock"></i>
+                                        </span>
                                     </p>
 
                                     @if ($errors->has('password'))
