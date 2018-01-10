19abd75e3aafc2165b714e3306bacb1d87b6510d
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-01-10 14:19:05 +0100

    Continue chart style

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,12 +2,45 @@
     @include('includes.head')
     <body>
         @include('includes/navigation')
-        <h3 id="top-250">Top 250 Movies Right Now</h3>
+        <h3 class="charts-titles">IMDb Charts</h3>
+        <h3 class="charts-titles">Top Rated Movies</h3>
+        <div class="dropdown">
+            <div class="dropdown-trigger">
+                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                <span>Dropdown button</span>
+                <span class="icon is-small">
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                <a href="#" class="dropdown-item">
+                    Dropdown item
+                </a>
+                <a class="dropdown-item">
+                    Other dropdown item
+                </a>
+                <a href="#" class="dropdown-item is-active">
+                    Active dropdown item
+                </a>
+                <a href="#" class="dropdown-item">
+                    Other dropdown item
+                </a>
+                <hr class="dropdown-divider">
+                <a href="#" class="dropdown-item">
+                    With a divider
+                </a>
+                </div>
+            </div>
+        </div>
+        <hr>
         <div class="grid--chart">
             @foreach($movies->slice(0, 20) as $movie) 
                 <div> 
-                    <img src="{{$movie->poster}}" height="185px"><br> 
+                    <img src="{{$movie->poster}}"><br> 
                     <b><a href="/movies/{{$movie->item_id}}">{{$movie->title}}</a></b>
+                    <img src="{{ asset('images/star.png') }}" height="22px" width="21px">
                     <p>Rating: {{$movie->rating}}</p> 
                 </div> 
             @endforeach 
