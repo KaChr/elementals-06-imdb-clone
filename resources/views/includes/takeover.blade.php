314a59c945c9eb6ea5b1d28b2360732d6289c2bd
Joakim Unge <ashba@unge.se>
2018-01-11 16:35:22 +0100

    Refactor takeover slightly

--- resources/views/includes/takeover.blade.php
+++ resources/views/includes/takeover.blade.php
@@ -1,16 +1,39 @@
 <section class="columns is-gapless">
+    <div class="column hide-overflow">
+      <a href="/movies/{{ $featured[1]->item_id }}">
+        <section class="hero takeover" style="background-image: url('{{$featured[1]->movieBackdrop}}')">
+          <div class="hero-body is-flex">
+              <div class="takeover__content--bottom is-flex">
+                <div class="takeover__info">
+                  <span class="takeover__info-genre">
+                  @foreach($item[1]->genres as $genre)
+                      {{ $loop->first ? '' : ', '}}
+                      {{$genre->genre_title}}
+                    @endforeach
+                    </span>
+                  <h1 class="takeover__info-title">{{ $featured[1]->title }}</h1>
+                </div>
+                <div class="takeover__cta is-flex">
+                    <button class="button button--small button--border-turquoise">WATCH TRAILER</button>
+                </div>
+              </div>
+          </div>
+        </section>
+      </a>
+    </div>
   <div class="column hide-overflow">
-      <section class="hero takeover" style="background-image: url('{{$featured[1]->movieBackdrop}}')">
+    <a href="/movies/{{ $featured[2]->item_id }}">
+      <section class="hero takeover" style="background-image: url('{{$featured[2]->movieBackdrop}}')">
         <div class="hero-body is-flex">
             <div class="takeover__content--bottom is-flex">
               <div class="takeover__info">
                 <span class="takeover__info-genre">
-                @foreach($item[1]->genres as $genre)
+                @foreach($item[2]->genres as $genre)
                     {{ $loop->first ? '' : ', '}}
                     {{$genre->genre_title}}
                   @endforeach
                   </span>
-                <h1 class="takeover__info-title">{{ $featured[1]->title }}</h1>
+                <h1 class="takeover__info-title">{{ $featured[2]->title }}</h1>
               </div>
               <div class="takeover__cta is-flex">
                   <button class="button button--small button--border-turquoise">WATCH TRAILER</button>
@@ -18,25 +41,6 @@
             </div>
         </div>
       </section>
-  </div>
-  <div class="column hide-overflow">
-    <section class="hero takeover" style="background-image: url('{{$featured[2]->movieBackdrop}}')">
-      <div class="hero-body is-flex">
-          <div class="takeover__content--bottom is-flex">
-            <div class="takeover__info">
-              <span class="takeover__info-genre">
-              @foreach($item[2]->genres as $genre)
-                  {{ $loop->first ? '' : ', '}}
-                  {{$genre->genre_title}}
-                @endforeach
-                </span>
-              <h1 class="takeover__info-title">{{ $featured[2]->title }}</h1>
-            </div>
-            <div class="takeover__cta is-flex">
-                <button class="button button--small button--border-turquoise">WATCH TRAILER</button>
-            </div>
-          </div>
-      </div>
-    </section>
+    </a>
   </div>
 </section>
