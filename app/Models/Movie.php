3b48b0b648ba9460334917fa0cbc6739e550dc11
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-12 12:02:13 +0100

    Add variables so admin can fill in details about a movie

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -23,6 +23,23 @@ class Movie extends Model
     // protected $hidden = [];
     // protected $dates = [];
 
+        protected $fillable = [
+        'item_id',
+        'title',
+        'summary',
+        'release_date',
+        'runtime',
+        'rating',
+        'poster'
+    ];
+
+    protected $primaryKey = 'item_id';
+    
+    public $incrementing = [false];
+
+    public function item(){
+        return $this->belongsTo('App\Item', 'items');
+    }
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
