d18beba03670d680a3c452bc442f139f6d0b4ef3
joakimunge <ashba@unge.se>
2018-01-15 14:32:27 +0100

    Clean up movie controller

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -53,18 +53,12 @@ class MoviesController extends Controller
     {
         $id = $movie->item_id;
         $movie = Movie::find($id);
-        // $reviews = Review::orderBy('created_at', 'desc')->where('item_id', $id)->limit(2)->get();
-
         $reviews = Review::orderBy('reviews.created_at', 'desc')
             ->where('reviews.item_id', $id)
             ->join('users', 'author_id', '=', 'users.id')
             ->join('movies', 'reviews.item_id', '=', 'movies.item_id')
             ->limit(2)->get(['reviews.*', 'users.name', 'movies.poster', 'reviews.rating AS review_rating']);
         
-        // item -> actors -> characters
-        // $item->actors as $actor
-        // $actor->character
-
         $item = Item::find($id)
                 ->leftJoin('actor_character_item', 'id', '=', 'actor_character_item.item_id')
                 ->leftJoin('characters as character', 'character.id', '=', 'actor_character_item.character_id')
@@ -72,8 +66,6 @@ class MoviesController extends Controller
                 ->where('item_id', $id)
                 ->get();
         
-        // dd($movie->poster);
-
         return view('movies.show', ['movie'=>$movie, 'item'=>$item, 'reviews'=>$reviews]);
     }
 
