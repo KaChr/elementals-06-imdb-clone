832f772838bf1197137773e39177800b012b8f5c
Karin Christensen <karin.christensen@chasacademy.se>
2018-01-16 14:55:44 +0100

    Add HTML

---
+++ resources/views/includes/search.blade.php
@@ -0,0 +1,4 @@
+<form action="" method="POST">
+    <input type="text" name="search" placeholder="Search">
+    <button class="button button--small button--border-turquoise" type="submit">SEARCH</button>
+</form>
