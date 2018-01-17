e4388b55572a30028bef851a1081753d32135467
Joakim Unge <ashba@unge.se>
2018-01-17 12:37:51 +0100

    Add form include to reviews

---
+++ resources/views/reviews/form.blade.php
@@ -0,0 +1,16 @@
+<div class="field">
+    <label class="label">Title</label>
+    <div class="control">
+        <input class="input" type="text" placeholder="e.g. ABSOLUTE MASTERPIECE">
+    </div>
+    </div>
+
+    <div class="field">
+    <label class="label">Review</label>
+    <div class="control">
+        <textarea class="textarea" placeholder="Write your review here"></textarea>
+    </div>
+    <div class="control">
+        <button class="button is-primary">{{ $submitText}}</button>
+    </div>
+</div>
