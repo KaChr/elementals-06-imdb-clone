5651b0653b86772d85db2bbd4054beda51ef5467
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-17 11:12:55 +0100

    Add custom login for every group member

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,56 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        //Delete users when seeding and then add new ones
+        DB::table('users')->delete();
+
+        //Add admin user when seeding
+        User::create(array(
+            'name' => 'Izabel', 
+            'email' => 'izabel@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+        User::create(array(
+            'name' => 'Joakim', 
+            'email' => 'joakim@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+        User::create(array(
+            'name' => 'Zav', 
+            'email' => 'zav@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+        User::create(array(
+            'name' => 'Gabriel', 
+            'email' => 'gabriel@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+        User::create(array(
+            'name' => 'Karin', 
+            'email' => 'karin@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+        User::create(array(
+            'name' => 'Kaveh', 
+            'email' => 'kaveh@emdb.com',
+            'password' => bcrypt('elem'),
+            'type' => 'admin'
+            ));
+
+
+
+        //Add 50 fake users
         factory(App\User::class, 50)->create();
+
     }
 }
