dcb1dfb476d0b1a04f2471e1c1b1a645b9f47488
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-18 16:02:44 +0100

    Create migration, model and controller for watchlist

---
+++ database/migrations/2018_01_18_104346_create_watchlists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWatchlistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watchlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+
+        //Schema::table('watchlists', function (Blueprint $table){
+        //    $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+        //});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watchlists');
+    }
+}
