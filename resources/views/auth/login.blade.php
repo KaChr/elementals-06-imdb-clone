eeb3af1992f2bc193d9409dc924c1ffcd86eb780
Joakim Unge <ashba@unge.se>
2018-01-19 14:07:36 +0100

    Add logo to login page and refactor some styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="columns is-marginless is-centered">
+    <div class="columns is-marginless is-centered v-center">
         <div class="column is-5">
             <div class="card">
-                <div class="card-content">
+                <div class="card-content is-centered">
+                    <div class="card__logo">
+                        <img id="splash__logo" src="{{ asset('images/emdb_logo@1x.png') }}">
+                    </div>
                     <form class="login-form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="field is-horizontal">
-                            <div class="field-label">
-                            </div>
-
                             <div class="field-body">
                                 <div class="field">
                                     <p class="control has-icons-left has-icons-right">
                                         <input class="input" id="email" type="email" name="email" placeholder="Email"
                                                value="{{ old('email') }}" required autofocus>
-                                               <span class="icon is-small is-left">
-                                                 <i class="fa fa-envelope"></i>
-                                                </span>
-                                                <span class="icon is-small is-right">
-                                                 <i class="fa fa-check"></i>
-                                                 </span>
+                                            <span class="icon is-small is-left">
+                                                <i class="fa fa-envelope"></i>
+                                            </span>
+                                            <span class="icon is-small is-right">
+                                                <i class="fa fa-check"></i>
+                                            </span>
                                     </p>
 
                                     @if ($errors->has('email'))
@@ -36,9 +35,6 @@
                         </div>
 
                         <div class="field is-horizontal">
-                            <div class="field-label">
-                            </div>
-
                             <div class="field-body">
                                 <div class="field">
                                     <p class="control has-icons-right">
@@ -47,7 +43,6 @@
                                          <i class="fa fa-lock"></i>
                                         </span>
                                     </p>
-
                                     @if ($errors->has('password'))
                                         <p class="help is-danger">
                                             {{ $errors->first('password') }}
@@ -58,8 +53,6 @@
                         </div>
 
                         <div class="field is-horizontal">
-                            <div class="field-label"></div>
-
                             <div class="field-body">
                                 <div class="field">
                                     <p class="control">
@@ -73,14 +66,11 @@
                         </div>
 
                         <div class="field is-horizontal">
-                            <div class="field-label"></div>
-
                             <div class="field-body">
                                 <div class="field">
                                     <div class="control feature__login">
-                                        <button type="submit" class="button__login button--big button--solid-turquoise">Login</button>
+                                        <button type="submit" class="button button__login button--big button--solid-turquoise">Login</button>
                                     </div>
-
                                     <div class="control feature__login login__remember">
                                         <a href="{{ route('password.request') }}">
                                             Forgot Your Password?
