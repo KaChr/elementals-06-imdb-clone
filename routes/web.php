344ee61dfe025a7a988350f5312cfa459fece9ec
Joakim Unge <ashba@unge.se>
2018-01-19 14:07:16 +0100

    Add login check to splash page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('splash');
-});
+
+Route::get('/', 'HomeController@splash')->name('splash');
 
 Route::get('/movie-api', function () {
     
@@ -28,18 +27,6 @@ Route::get('/omdb', function () {
 Route::get('/tvshowz', function () {
     
 return view('tvshowz');
-});
-
-Route::get('/profilepage', function () {
-    return view('profile-page');   
-});
-
-Route::get('/item', function () {
-    return view('item-page');   
-});
-
-Route::get('/login', function () {
-    return view('login');
 });
 
 Auth::routes();
