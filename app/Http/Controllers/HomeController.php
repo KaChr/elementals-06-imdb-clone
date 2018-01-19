56a9f3bcb75f012aca6363926b5cb558c86cb456
Joakim Unge <ashba@unge.se>
2018-01-19 16:05:24 +0100

    Get highest rated tv shows in homecontroller.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Movie;
 use App\Item;
 use App\Review;
+use App\Tvshow;
 use \Auth;
 use Illuminate\Http\Request;
 
@@ -35,12 +36,22 @@ class HomeController extends Controller
             $item[] = Item::find($feature->item_id);
         }
   
-        $spotlightMovies = Movie::orderBy('created_at', 'desc')->limit(5)->get();
-        $spotlightRated = Movie::orderBy('rating', 'desc')->limit(5)->get();  
+        $spotlightMovies = Movie::orderBy('movies.created_at', 'desc')
+        ->join('items', 'movies.item_id', '=', 'items.id')            
+        ->limit(5)->get();
+
+        $spotlightRated = Movie::orderBy('rating', 'desc')
+        ->join('items', 'movies.item_id', '=', 'items.id')            
+        ->limit(5)->get();
+
+        $spotlightTv = Tvshow::orderBy('rating', 'desc')
+        ->join('items', 'tvshows.item_id', '=', 'items.id')    
+        ->limit(5)->get();
         
         $spotlights = [
             'movies' => $spotlightMovies,
-            'rated' => $spotlightRated
+            'rated' => $spotlightRated,
+            'tvshows' => $spotlightTv
         ];
 
         $reviews = Review::orderBy('reviews.created_at', 'desc')
