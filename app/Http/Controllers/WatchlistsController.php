ac6a34664cd5a1a39a0d66f5b59f69823e3d143f
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-20 23:06:04 +0100

    User can add an item to watchlist, if it already exists in the list it wont add it.

--- app/Http/Controllers/WatchlistsController.php
+++ app/Http/Controllers/WatchlistsController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Watchlist;
 
+use App\User;
+
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
 
 class WatchlistsController extends Controller
@@ -23,4 +27,35 @@ class WatchlistsController extends Controller
         //
         return view ('watchlists.show', compact('watchlist'));
     }
+
+    public function store(Request $request)
+
+    {
+
+        //If a user wants to add item to watchlist
+        if (\Auth::user()){
+
+        $watchlist = Watchlist::where('user_id', '=', Auth::user()->id)
+        ->where('item_id', '=', $request->input('id'))->first();
+        
+        //If the user doesn't already have the item saved in watchlist: add it. 
+        if ($watchlist == null) {
+            $watchlist = Watchlist::create([
+                'user_id' => Auth::user()->id,
+                'item_id' => $request->input('id')
+            ]);
+            $watchlist->save();
+            return back();
+        }
+    }
+        
+        //Create a new watchlist using the request data
+
+        // Save it to the database
+
+        //And redirect...
+    }
+
 }
+
+
