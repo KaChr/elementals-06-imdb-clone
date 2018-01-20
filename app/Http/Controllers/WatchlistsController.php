1fe62099309b7018650755eb6d53c05961915fa6
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-20 23:06:04 +0100

    User can add an item to watchlist, if it already exists in the list it wont add it.

--- app/Http/Controllers/WatchlistsController.php
+++ app/Http/Controllers/WatchlistsController.php
@@ -3,26 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Watchlist;
-
-<<<<<<< HEAD
 use App\User;
-
 use Illuminate\Support\Facades\Auth;
-
-=======
->>>>>>> Create migration, model and controller for watchlist
 use Illuminate\Http\Request;
 
 class WatchlistsController extends Controller
 {
-<<<<<<< HEAD
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['show']]);
     }
 
-=======
->>>>>>> Create migration, model and controller for watchlist
     /*
     public function index()
 
@@ -35,7 +26,7 @@ class WatchlistsController extends Controller
     public function show(Watchlist $watchlist)
 
     {
-<<<<<<< HEAD
+
 
         $watchlists = Watchlist::with('movies')
         //->with('tvshows')
@@ -100,10 +91,37 @@ class WatchlistsController extends Controller
 
 }
 
-
-=======
         //
         return view ('watchlists.show', compact('watchlist'));
     }
+
+    public function store(Request $request)
+
+    {
+
+        //If a user wants to add item to watchlist
+        if (\Auth::user()){
+
+        $watchlist = Watchlist::where('user_id', '=', Auth::user()->id)
+        ->where('item_id', '=', $request->input('id'))->first();
+        
+        //If the user doesn't already have the item saved in watchlist: add it. 
+        if ($watchlist == null) {
+            $watchlist = Watchlist::create([
+                'user_id' => Auth::user()->id,
+                'item_id' => $request->input('id')
+            ]);
+            $watchlist->save();
+            return back();
+        }
+    }
+        
+        //Create a new watchlist using the request data
+
+        // Save it to the database
+
+        //And redirect...
+    }
+
 }
->>>>>>> Create migration, model and controller for watchlist
+
