b9ccf38ca7f22c81cd6ef753f177f9a1b5264324
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-20 23:39:53 +0100

    Add route to store a watchlist

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,4 @@ Route::post('/watchlist', 'WatchlistsController@store');
 Route::delete('/watchlist/delete', 'WatchlistsController@destroy');
 Route::get('watchlist', 'WatchlistsController@show')->middleware('auth');
 
+Route::post('/watchlist', 'WatchlistsController@store');
