2f72aaf6fa47173988959bd0f272b55e38a949f6
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-20 22:59:06 +0100

    Update watchlist table/model with a watched item. By default it is 0, user can change it to watched

--- app/Watchlist.php
+++ app/Watchlist.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Watchlist extends Model
 {
     //
+    protected $fillable = [
+        'item_id',
+        'user_id',
+        'watched'
+    ];
+
+    protected $primaryKey = 'item_id';
+
+    // public function item(){
+    //     return $this->belongsTo('App\Item', 'items');
+    // }
 }
