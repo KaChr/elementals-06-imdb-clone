8b204dae0a3c7983413ba9feed366ca33a1b32db
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-20 22:53:47 +0100

    Add post form when user wants to add item to watchlist

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -30,6 +30,23 @@
                             <a href="{{ route('movies.reviews.create', $movie)}}">
                                 <button class="button button--small button--border-blue" type="button">WRITE REVIEW</button>
                             </a>
+
+                        <form method="POST" action="/watchlist">
+                            {{ csrf_field() }}
+
+
+                            <input type="hidden" name="id" value="{{ $movie->item_id }}">
+
+                            <button class="button button--small button--border-blue" type="submit">
+
+                            
+                                <span class="icon">
+                                    <i class="fa fa-plus" area-hidden="true"></i>
+                                </span>
+                                WATCHLIST
+                            </button>
+                            </form>
+
                         </li>
                     </ul>
                     <span class="featured__info-genre">
