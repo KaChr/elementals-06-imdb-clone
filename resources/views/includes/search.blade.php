34bd9ee2afd7076722156ec044d10e2647d08fa3
Joakim Unge <ashba@unge.se>
2018-01-22 00:46:41 +0100

    Restructure search field and styling

--- resources/views/includes/search.blade.php
+++ resources/views/includes/search.blade.php
@@ -1,4 +1,9 @@
-<form action="/search" method="GET">
-    <input class="input" type="text" name="search" placeholder="Search">
-    <button class="button button--small button--border-turquoise" type="submit">SEARCH</button>
-</form>
+<section class="section columns is-centered">
+    <div class="column is-6-desktop">
+        <form action="/search" method="GET" class="search is-flex">
+            <h3 class="search__title">Looking for something?</h3>
+            <input class="input" type="text" name="search" placeholder="Search">
+            <button class="button button--big button--solid-turquoise search__button" type="submit">SEARCH</button>
+        </form>
+    </div>
+</section>
