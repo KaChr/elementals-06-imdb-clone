36d67592881f9153c0e8dbb993a36fb8b81e28dc
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-21 20:12:00 +0100

    Watchlist for mobile view is done

--- resources/views/watchlists/show.blade.php
+++ resources/views/watchlists/show.blade.php
@@ -3,34 +3,60 @@
 @section('content')
 
 <style>
-h2 {
+.watchlist {
     text-align: center;
 }
+
+
+
+.watchlist__poster, .watchlist__title  {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+}
+
+.watchlist__poster {
+    margin-top: 1rem;
+}
+
+.watchlist__title {
+    text-align: center;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+}
+
+.watchlist__release, .wathclist__runtime, .watchlist__rating {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+}
+
+.watchlist__summary {
+    margin: 2rem;
+    margin-top: 1rem;
+}
+
 </style>
 
 
-<h2>Watchlist </h2>
+<h2 class="watchlist">Watchlist </h2>
 
 @foreach ($watchlists as $watchlist)
 
    
-   <div class="watchlist"> 
-        <a href ="/movies/ {{$watchlist->movies->implode('item_id')}}">
-            <img src="{{ $watchlist->movies->implode('poster', ',') }}">
-                <h3> {{ $watchlist->movies->implode('title', ',') }} </h3>
+   <div class="watchlist__wrapper"> 
+        <a class="watchlist__id" href ="/movies/ {{$watchlist->movies->implode('item_id')}}">
+            <img class="watchlist__poster" src="{{ $watchlist->movies->implode('poster', ',') }}">
+                <h3 class="watchlist__title"> {{ $watchlist->movies->implode('title', ',') }} </h3>
         </a>
 
-        <h5> {{ $watchlist->movies->implode('release_date', ',') }} 
-          
-         {{ $watchlist->movies->implode('runtime', ',') }} 
-         {{ $watchlist->movies->implode('rating', ',') }}</h5>
-        <p> {{ $watchlist->movies->implode('summary', ',') }} </p>
-        
-    </div>
-        
-         
-        
-     
+        <h5 class="watchlist__release"> {{ $watchlist->movies->implode('release_date', ',') }} </h5>
+        <h5 class="wathclist__runtime"> {{ $watchlist->movies->implode('runtime', ',') }} </h5>
+        <h5 class="watchlist__rating"> {{ $watchlist->movies->implode('rating', ',') }} </h5>
+        <p class="watchlist__summary"> {{ $watchlist->movies->implode('summary', ',') }} </p>
+
+    </div>     
     
 
 @endforeach
