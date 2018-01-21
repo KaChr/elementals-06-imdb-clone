a2da1b44daefbd82ed7ae1ba8a01b624a63cd441
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-01-21 18:35:33 +0100

    Logged in user can view all movies in watchlist and go direct to a movie

--- resources/views/watchlists/show.blade.php
+++ resources/views/watchlists/show.blade.php
@@ -1,1 +1,40 @@
 @extends('layouts.app')
+
+@section('content')
+
+<style>
+h2 {
+    text-align: center;
+}
+</style>
+
+
+<h2>Watchlist </h2>
+
+@foreach ($watchlists as $watchlist)
+
+   
+   <div class="watchlist"> 
+        <a href ="/movies/ {{$watchlist->movies->implode('item_id')}}">
+            <img src="{{ $watchlist->movies->implode('poster', ',') }}">
+                <h3> {{ $watchlist->movies->implode('title', ',') }} </h3>
+        </a>
+
+        <h5> {{ $watchlist->movies->implode('release_date', ',') }} 
+          
+         {{ $watchlist->movies->implode('runtime', ',') }} 
+         {{ $watchlist->movies->implode('rating', ',') }}</h5>
+        <p> {{ $watchlist->movies->implode('summary', ',') }} </p>
+        
+    </div>
+        
+         
+        
+     
+    
+
+@endforeach
+
+
+
+@endsection
