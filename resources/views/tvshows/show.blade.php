8de99f03a02c21ac1164b9097154a86f3ae6075f
Joakim Unge <ashba@unge.se>
2018-01-21 20:58:14 +0100

    Add show-view for tvshows

--- resources/views/tvshows/show.blade.php
+++ resources/views/tvshows/show.blade.php
@@ -1,44 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title> 
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
 
-    <h1>{{$tvshow->title}}</h1>
-    <img src="{{$tvshow->poster}}">
-    <img src="{{$tvshow->tvBackdrop}}">
-    <p>{{$tvshow->rating}}</p>
-    <p>{{$tvshow->summary}}</p>
-    <p>{{$tvshow->runtime}}</p>
-    <p>{{$tvshow->countries}}</p>
-    <h2>Genres</h2>
-    <p>
-    @foreach($item->genres as $genre)
-        {{ $loop->first ? '' : ', '}}
-        {{$genre->genre_title}}
-    @endforeach
-    </p>
-    @foreach($seasons as $season)
-        <a href="{{$item->id}}/seasons/{{$season->season_nr}}">Season {{$season->season_nr}}</a><br>
-    @endforeach
-    <h2>Actors</h2>
-    @foreach($item->actors as $actor)
-        @foreach($item->characters as $character)
-            @foreach($character->actor as $actor)
-            <p>{{$actor->name}} as {{$character->character}}</p>
-            <img src="{{$actor->profile_pic}}">
-            @endforeach
+<div class="item">
+    <section class="hero featured" style="background-image: url('{{$tvshow->tvBackdrop}}')">
+        <div class="hero-body is-flex">
+            <div class="featured__content--bottom is-flex">
+                <div class="featured__info">
+                    @include('includes.rating', ['rating' => $tvshow->rating])       
+                    <p class="featured__info-year">2014</p>
+                    <p class="featured__info-runtime">{{$tvshow->runtime}}</p>
+                    <h1 class="featured__info-title">{{$tvshow->title}}</h1>
+                    <ul class="featured__info__actions">
+                        <li>
+                            <a href="#">
+                                <button class="button button--small button--border-blue" type="button">
+                                    <span class="icon">
+                                        <i class="fa fa-plus" area-hidden="true"></i>
+                                    </span>
+                                    WATCHLIST
+                                </button>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <button class="button button--small button--border-blue" type="button">TRAILER</button>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('tvshows.reviews.create', $tvshow)}}">
+                                <button class="button button--small button--border-blue" type="button">WRITE REVIEW</button>
+                            </a>
+                        </li>
+                    </ul>
+                    <span class="featured__info-genre">
+                        @foreach($item[0]->genres as $genre)
+                            {{ $loop->first ? '' : ', '}}
+                            {{$genre->genre_title}}
+                        @endforeach
+                    </span>
+                </div>
+            </div>
+        </div> 
+    </section>
+    <section class="plot">
+        <div class="container">
+            <img class="plot__poster-img" src="{{$tvshow->poster}}" alt="Movie poster">
+            <article class="plot__text">
+                <h4>PLOT SUMMARY</h4>
+                <p>{{$tvshow->summary}}</p>
+                <button class="button button--small button--outline-blue" type="button">READ MORE</button>
+            </article>
+        </div>
+    </section>
+    <section class="cast-crew">
+        <div class="container">
+            @include('includes.person', [
+                'actors' => $item[0]->actors, 
+                'directors' => $item[0]->directors
+            ])
+        </div>
+    </section>
+    <section class="section reviews">
+        <div class="container">
+            @include('includes.reviews', ['reviews' => $reviews, 'poster' => $tvshow->poster])
+        </div>
+    </section>
+
+    <section class="section">   
+        @foreach($seasons as $season)
+            <a href="{{$item[0]->item_id}}/seasons/{{$season->season_nr}}">Season {{$season->season_nr}}</a><br>
         @endforeach
-        @break
-    @endforeach
-    <h2>Directors</h2>
-    @foreach($item->directors as $director)
-        <p>{{$director->name}}</p>
-        <img src="{{$director->profile_pic}}">
-    @endforeach 
-</body>
-</html>
+    </section>
+</div>
+
+
+@endsection
