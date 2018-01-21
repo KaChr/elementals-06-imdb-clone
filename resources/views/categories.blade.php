e155596aebc7884706833317338e543590468389
Keejv <kaveh.haddad@chasacademy.se>
2018-01-21 20:32:15 +0100

    Add genre select filter

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 @section('content')
-        <h3 class="charts__titles">IMDb Charts</h3>
-        <h3 class="charts__titles">Top Rated Movies</h3>
+        <h3 class="charts__titles">Movie Categories</h3>
         <hr>
-        <div id="chart-buttons">
-            <h6>Sort By</h6>
-            <span>@sortablelink('title')</span>
-            <span>@sortablelink('rating')</span>
-            <span>@sortablelink('release_date')</span>
+        <form method='post' action='/categories'>
+            {{ csrf_field() }}
+            <div class="select">
+                <select name='genre'>
+                    @foreach($genres as $genre)
+                        <option value="{{ $genre->id }}">{{ $genre->genre_title }}</option>
+                    @endforeach
+                </select>
+                <input type="submit" value="CHOOSE">
+            </div>
+        </form>
         </div>
             <ol>
             @foreach($movies as $movie)
