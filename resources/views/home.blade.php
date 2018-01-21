9b7f4c7bd27023dbfb8322db69c4cccfe6dcfeed
Joakim Unge <ashba@unge.se>
2018-01-22 00:46:49 +0100

    Move search field position

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     @include('includes.hero')
-    @include('includes.search')
     @include('includes.takeover')
     <div class="container">
+        @include('includes.search')
         @include('includes.spotlight', ['title' => 'TOP RATED MOVIES', 'spotlights' => $spotlights['rated']])
         @include('includes.spotlight', ['title' => 'RECENTLY ADDED MOVIES', 'spotlights' => $spotlights['movies']])
         @include('includes.spotlight', ['title' => 'RECENTLY ADDED TV SHOWS', 'spotlights' => $spotlights['tvshows']])                        
