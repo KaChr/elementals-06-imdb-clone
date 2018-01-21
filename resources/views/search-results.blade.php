837530ec1e07488da7c80c2131f483e29662c00e
Joakim Unge <ashba@unge.se>
2018-01-22 00:47:24 +0100

    Change include in search results to new file

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -3,21 +3,21 @@
 
 @include('includes.search')
 
-@foreach ($results as $key => $result)
-<div class="container">
-    @includeWhen(!$result->isEmpty(), 'includes.spotlight', [
-        'title' => 'SEARCH RESULTS FOR “' . $query . '” IN ' . strtoupper($key), 
-        'spotlights' => $results[$key],
-        'type' => $key
-    ])
-    
-    @includeWhen($results, 'includes.spotlight', [
-        'title' => 'NO RESULTS FOUND FOR “' . $query . '”', 
-        'spotlights' => [],
-        'type' => $key
-    ])
+    @foreach ($results as $key => $result)
+    <div class="container">
+        @includeWhen(!$result->isEmpty(), 'includes.results', [
+            'title' => 'SEARCH RESULTS FOR “' . $query . '” IN ' . strtoupper($key), 
+            'spotlights' => $results[$key],
+            'type' => $key
+        ])
+        
+        @includeWhen($results, 'includes.results', [
+            'title' => 'NO RESULTS FOUND FOR “' . $query . '”', 
+            'spotlights' => [],
+            'type' => $key
+        ])
 
-</div>
-@endforeach
+    </div>
+    @endforeach
 
 @endsection
