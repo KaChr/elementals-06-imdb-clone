4fba3bab4b346063a8aa351e55bcf73d5c477907
Joakim Unge <ashba@unge.se>
2018-01-23 23:33:19 +0100

    Register admin middleware in service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -55,6 +55,8 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+        
+        Route::aliasMiddleware('admin', \App\Http\Middleware\IsAdmin::class);
     }
 
     /**
